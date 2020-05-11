4c2bc203ff500b6c3054ab29d54be156f9b30fbd
CasperDeduct <ruslan.masuk@gmail.com>
2020-05-11 21:58:22 +0300

    22222222222222

--- bundles/cloud/src/main/java/org/touchhome/bundle/cloud/setting/CloudServerPortSetting.java
+++ bundles/cloud/src/main/java/org/touchhome/bundle/cloud/setting/CloudServerPortSetting.java
@@ -6,7 +6,7 @@ public class CloudServerPortSetting implements BundleSettingPlugin<Integer> {
 
     @Override
     public SettingType getSettingType() {
-        return SettingType.Text;
+        return SettingType.Integer;
     }
 
     @Override
