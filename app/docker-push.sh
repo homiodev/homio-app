027baf696461aa56ed3db1018c89f4c987531ab4
CasperDeduct <ruslan.masuk@gmail.com>
2020-05-18 21:28:19 +0300

    test bintray

--- app/docker-push.sh
+++ app/docker-push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 docker login -u ${DOCKER_USERNAME} -p ${DOCKER_PASSWORD}
-docker build -t ruslanmasuk1985/touchhome-core:$1 .
-docker push ruslanmasuk1985/touchhome-core:$1
+docker build -t touchhome/core:$1 .
+docker push touchhome/core:$1
