b0d7762300e9d89984abb9f031a028ea7e6351eb
CasperDeduct <ruslan.masuk@gmail.com>
2020-05-19 15:57:36 +0300

    test bintray

--- app/src/main/java/org/touchhome/app/hardware/HotSpotHardwareRepository.java
+++ app/src/main/java/org/touchhome/app/hardware/HotSpotHardwareRepository.java
@@ -8,10 +8,10 @@ import org.touchhome.bundle.api.hardware.api.HardwareRepositoryAnnotation;
 public interface HotSpotHardwareRepository {
     @HardwareQuery(echo = "Disable Hostapd", value = "systemctl disable hostapd", ignoreOnError = true)
     @HardwareQuery(echo = "Disable Dnsmasq", value = "systemctl disable dnsmasq", ignoreOnError = true)
-    @HardwareQuery(echo = "Copy required files", printOutput = true, value = "mkdir -p /etc/hostapd" +
-            "cp -n :sysDir/hostapd.conf /etc/hostapd/hostapd.conf &&" +
-            "cp -n :sysDir/dnsmasq.conf /etc/dnsmasq.conf &&" +
-            "cp -n :sysDir/autohotspot.service /etc/systemd/system/autohotspot.service &&" +
+    @HardwareQuery(echo = "Copy required files", printOutput = true, value = "mkdir -p /etc/hostapd && " +
+            "cp -n :sysDir/hostapd.conf /etc/hostapd/hostapd.conf && " +
+            "cp -n :sysDir/dnsmasq.conf /etc/dnsmasq.conf && " +
+            "cp -n :sysDir/autohotspot.service /etc/systemd/system/autohotspot.service && " +
             "cp -n :sysDir/autohotspot /usr/bin/autohotspot")
     @HardwareQuery(echo = "Enable AutoHotSpot Service", value = "systemctl enable autohotspot.service", printOutput = true)
     @HardwareQuery(echo = "Set AutoHotSpot executable", value = "chmod +x /usr/bin/autohotspot", printOutput = true)
