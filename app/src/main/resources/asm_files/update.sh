3b3e7c6d36615e10fa071d4ed98030fc21d733e4
CasperDeduct <ruslan.masuk@gmail.com>
2020-05-20 15:01:10 +0300

    test bintray

--- app/src/main/resources/asm_files/update.sh
+++ app/src/main/resources/asm_files/update.sh
@@ -1,20 +1,31 @@
 #!/bin/bash
 
-VERSION=$(curl https://api.github.com/repos/touchhome/touchHome-core/releases/latest | python -c "import sys,json; print json.load(sys.stdin)['name']")
+VERSION=$(curl https://api.github.com/repos/touchhome/touchHome-core/releases/latest | python -c "import sys,json; print json.load(sys.stdin)['tag_name']")
 echo "Latest app version: $VERSION"
 PROGRAM_NAME="touchHome"
-PROGRAM_PATH=$HOME/${PROGRAM_NAME}.jar
+PROGRAM_PATH=/opt/${PROGRAM_NAME}.jar
 EXPECTED_CHECKSUM="`wget -qO- https://github.com/touchhome/touchHome-core/releases/download/${VERSION}/touchHome.jar.md5`"
 RELEASE_PROGRAM_URL="https://github.com/touchhome/touchHome-core/releases/download/${VERSION}/touchHome.jar"
+
+PROGRAM_NAME="touchHome"
+PID=`ps aux | grep -v -e 'grep ' | grep ${PROGRAM_NAME} | tr -s " " | cut -d " " -f 2`
+
 if [ -f "$PROGRAM_PATH" ]; then
     ACTUAL_CHECKSUM=`md5sum ${PROGRAM_PATH} | awk '{ print $1 }'`
     if [ "$ACTUAL_CHECKSUM" == "$EXPECTED_CHECKSUM" ]; then
        echo "Program already updated"
+       if ! [ -z "${PID}" ]; then
+          echo "App already started"
+       else
+          echo "Starting app..."
+          nohup sudo java -jar ${PROGRAM_PATH} &>/dev/null &
+       fi
        exit 0
     fi
 fi
 
-PROGRAM_RELEASE_PATH=$HOME/${PROGRAM_NAME}.tmp
+
+PROGRAM_RELEASE_PATH=/opt/${PROGRAM_NAME}.tmp
 rm -f ${PROGRAM_RELEASE_PATH}
 echo "Downloading release: ${RELEASE_PROGRAM_URL}"
 wget -q ${RELEASE_PROGRAM_URL} -O ${PROGRAM_RELEASE_PATH}
@@ -26,8 +37,6 @@ if [ "$ACTUAL_CHECKSUM" != "$EXPECTED_CHECKSUM" ]; then
 fi
 
 
-PROGRAM_NAME="touchHome"
-PID=`ps -ef | grep ${PROGRAM_NAME} | awk '{print $2}'`
 if ! [ -z "${PID}" ]; then
    echo "Killing old program '${PID}'"
    sudo kill ${PID}
