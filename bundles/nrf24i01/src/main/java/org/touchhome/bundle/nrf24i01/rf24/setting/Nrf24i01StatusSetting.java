1eb53b1b13d7e23d77fe0d7e27380283c910eddf
CasperDeduct <ruslan.masuk@gmail.com>
2020-06-24 12:31:39 +0300

    prepare to 0.1.8

--- bundles/nrf24i01/src/main/java/org/touchhome/bundle/nrf24i01/rf24/setting/Nrf24i01StatusSetting.java
+++ bundles/nrf24i01/src/main/java/org/touchhome/bundle/nrf24i01/rf24/setting/Nrf24i01StatusSetting.java
@@ -1,6 +1,8 @@
 package org.touchhome.bundle.nrf24i01.rf24.setting;
 
+import org.apache.commons.lang.StringUtils;
 import org.touchhome.bundle.api.BundleSettingPlugin;
+import org.touchhome.bundle.api.EntityContext;
 import org.touchhome.bundle.api.model.DeviceStatus;
 
 public class Nrf24i01StatusSetting implements BundleSettingPlugin<DeviceStatus> {
@@ -12,5 +14,10 @@ public class Nrf24i01StatusSetting implements BundleSettingPlugin<DeviceStatus> 
     @Override
     public int order() {
         return 2;
+    }
+
+    @Override
+    public DeviceStatus parseValue(EntityContext entityContext, String value) {
+        return StringUtils.isEmpty(value) ? null : DeviceStatus.valueOf(value);
     }
 }
